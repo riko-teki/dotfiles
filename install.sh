faf8eb15d2c48909d8c4714780e07b59dd6a4e1d
riko-teki <boiledshrimps0109@gmail.com>
2022-06-07 15:26:24 +0900

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-echo "Installing dotfiles..."
+echo -e "Installing dotfiles...\n"
+echo "HOME=$HOME"
 
-echo "Creating symbolic link..."
+CURRENT="$(cd $(dirname $0); pwd)"
+echo "CURRENT=$CURRENT"
 
-ln -s ./bash/.bashrc "$HOME/.bashrc"
-ln -s ./zsh/.zshrc "$HOME/.zshrc"
-ln -s ./fish/.fishrc "$HOME/.fishrc"
-ln -s ./nvim/ "$HOME/.config/nvim"
+work_files=`find ./ -maxdepth 1 -type f \
+                    -not -name \*.sh \
+		    -and -not -name \*.ps1 \
+		    -and -not -name \*.md \
+		    -and -not -name \*.git\* | sed 's!^.*/!!'`
 
+echo "##### dotfiles #####"
+for file in $work_files;
+do
+	if [ ! -e $HOME/$file ] && [ ! -L $HOME/$file ]; then
+		echo "[+] cleate symlink $file -> $HOME/$file"
+		ln -s $CURRENT/$file $HOME/$file
+	else
+		echo -e "[+] $file is already exists.\n"
+	fi
+done
+
+echo "##### nvim config #####"
+NVIM="$HOME/.config/nvim"
+CONFIG="$HOME/.config"
+
+if [ ! -d $NVIM ] && [ ! -L $NVIM ]; then
+	if [ ! -d $CONFIG ]; then
+		mkdir $CONFIG
+	fi
+	echo "[+] cleate symlink $NVIM -> $NVIM"
+	ln -s $CURRENT/nvim $HOME
+else
+	echo -e "[+] $NVIM is already exists.\n"
+fi
+echo "##### install nvim #####"
+sudo apt install -y neovim
+
+echo -e "\n"
 echo "Finished installing dotfiles"
